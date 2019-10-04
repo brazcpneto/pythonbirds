9ff5032088dfcb001c32217f63b3115e0d5c18e5
brazcpneto@gmail.com <Z0m@0$LLST3L>
2019-10-04 15:28:31 -0300

    ...

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,42 +1,24 @@
 class Pessoa:
     olhos = 2
 
-    def __init__(self, *filhos, nome = None, idade = 28):
-        self.idade = idade
+    def __init__(self, *filhos, nome = None, idade = None):
         self.nome = nome
+        self.idade = idade
         self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
     @staticmethod
-    def metodo_estatico():
-        return 42
+    def metodo_estatico(a, b):
+        return a + b
 
     @classmethod
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
-    renzo = Pessoa(nome = 'Renzo')
-    luciano = Pessoa(renzo, nome ='Luciano')
-    print(luciano.cumprimentar())
-    print(id(luciano))
-    print(luciano.nome)
-    print(luciano.idade)
-    for filho in luciano.filhos:
-        print(filho.nome)
+    renzo = Pessoa(nome = 'Renzo', idade = 30)
+    luciano = Pessoa(renzo, nome = 'Luciano', idade = 40)
     luciano.sobrenome = 'Ramalho'
-    del luciano.filhos
-    luciano.olhos = 1
-    del luciano.olhos
-    print(luciano.__dict__)
-    print(renzo.__dict__)
-    Pessoa.olhos = 3
-    print(Pessoa.olhos)
-    print(luciano.olhos)
-    print(renzo.olhos)
-    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
-    print(Pessoa.metodo_estatico())
-    print(luciano.metodo_estatico())
-    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    print(Pessoa.nome_e_atributos_de_classe())
